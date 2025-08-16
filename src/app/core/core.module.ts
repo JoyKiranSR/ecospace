f7ab93a18e608ae62ef5fdd43f74dbc45d4dda02
Joy Kiran S R <srjoykiran@gmail.com>
2025-08-16 18:57:23 +0530

    Added sidebar component within a core module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    HeaderComponent
+    HeaderComponent,
+    SidebarComponent
   ],
-  exports: [HeaderComponent]
+  exports: [
+    HeaderComponent,
+    SidebarComponent
+  ]
 })
 export class CoreModule { }
