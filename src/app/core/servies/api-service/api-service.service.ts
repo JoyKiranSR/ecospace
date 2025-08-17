f5087f5ee8d16731f4053a12adf831b1f29c741d
Joy Kiran S R <srjoykiran@gmail.com>
2025-08-18 01:57:50 +0530

    Added generic get request method

--- src/app/core/servies/api-service/api-service.service.ts
+++ src/app/core/servies/api-service/api-service.service.ts
@@ -1,6 +1,9 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, map, Observable } from 'rxjs';
+
+import { ApiPagination, ApiResponse, ApiSuccessResponse } from '../../../models/api.model';
+import { toCamelCase } from '../../utils/api-utility';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +13,22 @@ export class ApiService {
 
   constructor(private http: HttpClient) { }
 
-  get<ApiResponse>(endpoint: string, params?: Record<string, any>): Observable<ApiResponse> {
-    const httpParams = new HttpParams({ fromObject: params || {} });
-    return this.http.get<ApiResponse>(`${this.baseUrl}/${endpoint}`, { params: httpParams });
+  /**
+   * Generic GET request method
+   * @param endpoint - The API endpoint to call
+   * @param params - Optional query parameters
+   * @returns Observable of the API response
+   */
+  get<T>(endpoint: string, params?: Record<string, any>): Observable<ApiSuccessResponse<T>> {
+    const url = `${this.baseUrl}/${endpoint}`;
+    const httpParams = new HttpParams({ fromObject: params ?? {} });
+    return this.http.get<ApiResponse<T>>(url, { params: httpParams })
+      .pipe(
+        // Handle any additional transformations or error handling here if needed
+        map(
+          (response: ApiResponse<T>) => (
+            { data: toCamelCase(response.data), pagination: response.pagination } as ApiSuccessResponse<T>
+          )),
+      );
   }
 }
