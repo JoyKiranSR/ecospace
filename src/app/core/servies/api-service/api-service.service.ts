9402a15305e30af9c56f0b4b7d8f4d7b3d3d622f
Joy Kiran S R <srjoykiran@gmail.com>
2025-08-17 21:17:41 +0530

    Added api-service with get request for all backend API calls

---
+++ src/app/core/servies/api-service/api-service.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiServiceService {
+  private baseUrl = 'http://localhost:7000'; // Base URL for the API
+
+  constructor(private http: HttpClient) { }
+
+  get<T>(endpoint: string, params?: Record<string, any>): Observable<T> {
+    const httpParams = new HttpParams({ fromObject: params || {} });
+    return this.http.get<T>(`${this.baseUrl}/${endpoint}`, { params: httpParams });
+  }
+}
