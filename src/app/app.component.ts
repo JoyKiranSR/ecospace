c0026fc8043fe8df42e31a41ee99644e58a6b475
Joy Kiran S R <srjoykiran@gmail.com>
2025-08-17 20:56:38 +0530

    Seperated Layout from app component into a dedicated component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CoreModule } from './core/core.module';
-import { SharedModule } from './shared/shared.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    CoreModule,
-    RouterOutlet,
-    SharedModule
+    RouterOutlet
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
