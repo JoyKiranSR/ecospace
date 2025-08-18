24b2d28d9dfc8c7669dcdae873c7d4c0eecf80b5
Joy Kiran S R <srjoykiran@gmail.com>
2025-08-18 15:08:04 +0530

    Updated app routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './core/components/layout/layout.component';
+import { SoilListComponent } from './features/soils/pages/soil-list/soil-list.component';
+import { SoilDetailsComponent } from './features/soils/pages/soil-details/soil-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: 'soils', component: SoilListComponent },
+      { path: 'soils/:id', component: SoilDetailsComponent },
+      { path: '', redirectTo: 'soils', pathMatch: 'full' },
+      { path: '**', redirectTo: 'soils' } // Redirect any unknown paths to plants
+    ]
+  }
+];
