094ba88084227b54414047cf0a8fed6a27043a1a
Joy Kiran S R <srjoykiran@gmail.com>
2025-09-17 14:04:04 +0530

    Added new route for details page of fertilizers which loads its template component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { LayoutComponent } from './core/components/layout/layout.component';
 import { SoilListComponent } from './features/soils/pages/soil-list/soil-list.component';
 import { SoilDetailsComponent } from './features/soils/pages/soil-details/soil-details.component';
 import { FertilizerListComponent } from './features/fertilizers/pages/fertilizer-list/fertilizer-list.component';
+import { FertilizerDetailsComponent } from './features/fertilizers/pages/fertilizer-details/fertilizer-details.component';
 
 export const routes: Routes = [
   {
@@ -12,6 +13,7 @@ export const routes: Routes = [
       { path: 'soils', component: SoilListComponent },
       { path: 'soils/:id', component: SoilDetailsComponent },
       { path: 'fertilizers', component: FertilizerListComponent },
+      { path: 'fertilizers/:id', component: FertilizerDetailsComponent },
       { path: '', redirectTo: 'soils', pathMatch: 'full' },
       { path: '**', redirectTo: 'soils' } // Redirect any unknown paths to plants
     ]
---
+++ src/app/features/fertilizers/pages/fertilizer-details/fertilizer-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fertilizer-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './fertilizer-details.component.html',
+  styleUrl: './fertilizer-details.component.scss'
+})
+export class FertilizerDetailsComponent {
+
+}
