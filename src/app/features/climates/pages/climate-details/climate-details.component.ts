ccc776aa90150f15fbae40cdbbf78882ef9bd3b8
Joy Kiran S R <srjoykiran@gmail.com>
2025-09-20 23:24:20 +0530

    Updatd climate details component to show header and no data logic

--- src/app/features/climates/pages/climate-details/climate-details.component.ts
+++ src/app/features/climates/pages/climate-details/climate-details.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { Climate } from '../../models/climate.model';
+import { ListPageWrapperComponent } from '../../../../shared/components/list-page-wrapper/list-page-wrapper.component';
 
 @Component({
   selector: 'app-climate-details',
   standalone: true,
-  imports: [],
+  imports: [
+    ListPageWrapperComponent
+  ],
   templateUrl: './climate-details.component.html'
 })
 export class ClimateDetailsComponent {
+  climate: Climate | null;
+  pageWrapper: { title: string, description: string, instruction?: string };
 
+  constructor() {
+    // Initialize properties
+    this.climate = null;
+    this.pageWrapper = {
+      title: 'Climate Details',
+      description: 'View detailed information about a specific climate.'
+    };
+  }
 }
